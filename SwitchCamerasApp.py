82eaff224b43162311935caf2af6a304062a9591
Haigang <32087046+learningCCC@users.noreply.github.com>
2022-06-14 21:42:40 -0700

    Update SwitchCamerasApp.py
    Add camera index to CamOne and CamTwo class

--- SwitchCamerasApp.py
+++ SwitchCamerasApp.py
@@ -7,20 +7,36 @@ Created on Fri Jun 10 11:14:09 2022
 
 import tkinter as tk
 from tkinter import *
-from  CameraApp_tkinter_main import TkCamera 
+from CameraApp_tkinter_main import *
+from GetCamIndex import *
 
-class SampleApp(tk.Tk):
+
+class GetCamIndex:
+    indexes = None
+    def __init__(self):
+        GetCamIndex.indexes = self.getIndex()
+        
+    def getIndex(self):
+        index_cls = CamIndex()
+        indexes = index_cls.get_cam_indexes()
+        return indexes
+    
+class CameraSwitchApp(tk.Tk, GetCamIndex):
     def __init__(self):
         tk.Tk.__init__(self)
+ 
         self._mainCam= None
         # The dictionary to hold the class type
         self._allCams = dict()
         # Switch cameras
         self.switch_Cam(CamOne)
         # closing windows release cameras
+
         self.protocol('WM_DELETE_WINDOW', self.on_closing)
         self.mainloop()
+        
 
+    
     def switch_Cam(self, Cam_class):
         # hide the current Frame if exists
         if self._mainCam:
@@ -36,45 +52,50 @@ class SampleApp(tk.Tk):
             self._allCams[Cam_class] = Cam
 
         # Pack the Cam or self._mainCam (these are all frames)
-        print(self._allCams)
         Cam.grid(row=0, column=0,sticky=N+S+W+E)
-        
-        
         Cam.bind('<Configure>', Cam.frame.resize_image)
              
         self.rowconfigure(0, weight=1) #noqa
-        self.columnconfigure(0, weight=1) #noqa
-        
+        self.columnconfigure(0, weight=1) #noqa 
         # and make it the 'default' or current one.
         self._mainCam = Cam
         
     def on_closing(self, event=None):
-        for i, cam in self._allCams.items():
-            cam.frame.vid.running = False
-            cam.frame.release_cam()
+        if self._allCams!= None:
+            for i, cam in self._allCams.items():
+                cam.frame.vid.running = False
+                cam.frame.release_cam()
         self.destroy()
-
-class CamOne(tk.Frame):
-    def __init__(self, master, *args, **kwargs):
+        
+class CamOne(tk.Frame, GetCamIndex):
+    def __init__(self, master,  *args, **kwargs):
         tk.Frame.__init__(self,master, *args, **kwargs)
         
         #(4096, 2160,30), (1920, 1080,60) for logitech
         width = 4096
         height = 2160
-
-        self.frame = TkCamera(self, 'First Camera', 0, width,  height, 15)
-        #self.frame.pack(fill="both",expand=True)
-        self.frame.grid(row=0, column=0, sticky=N+S+W+E) #noqa
-        self.frame.bind('<Configure>', self.frame.resize_image)
-             
-        self.button = tk.Button(self, text="To cam two",
-              command=lambda: master.switch_Cam(CamTwo))
-        self.button.grid(row=1,column=0)
+        indexes = GetCamIndex.indexes
+        if len(indexes) == 0: return
         
-        self.rowconfigure(0, weight=1) #noqa
-        self.columnconfigure(0, weight=1) #noqa
+        self.frame = None
+        try:
+            self.frame = TkCamera(self, 'First Camera',indexes[0], width,  height, 15)
+        except:
+            print('unable to open campera one')
+       
+        
+        if self.frame:
+            self.frame.grid(row=0, column=0, sticky=N+S+W+E) #noqa
+            #self.frame.bind('<Configure>', self.frame.resize_image)
+            if len(indexes) == 2:  
+                self.button = tk.Button(self, text="To cam two",
+                      command=lambda: master.switch_Cam(CamTwo))
+                self.button.grid(row=1,column=0)
+                
+            self.rowconfigure(0, weight=1) #noqa
+            self.columnconfigure(0, weight=1) #noqa
 
-class CamTwo(tk.Frame): # Sub-lcassing tk.Frame
+class CamTwo(tk.Frame, GetCamIndex): # Sub-lcassing tk.Frame
     def __init__(self, master, *args, **kwargs):
         # self is now an istance of tk.Frame
         tk.Frame.__init__(self,master, *args, **kwargs)
@@ -82,17 +103,27 @@ class CamTwo(tk.Frame): # Sub-lcassing tk.Frame
         # make a new Cam whose parent is self.
         width = 4096
         height = 2160
-
-        self.frame = TkCamera(self, '2nd Camera', 1, width,  height, 15)
-        #self.frame.pack(fill="both",expand=True)
-        self.frame.grid(row=0, column=0, sticky=N+S+W+E) #noqa
-        self.rowconfigure(0, weight=1) #noqa
-        self.columnconfigure(0, weight=1) #noqa
         
-        self.button = tk.Button(self, text="To cam one",
-              command=lambda: master.switch_Cam(CamOne))
-        self.button.grid(row=1,column=0)
+        indexes = GetCamIndex.indexes
+        self.frame = None
+        try:
+            self.frame = TkCamera(self, 'First Camera',indexes[1], width,  height, 15)
+        except:
+            print('unable to open campera two')
+        if self.frame: 
+            self.frame.grid(row=0, column=0, sticky=N+S+W+E) #noqa
+            self.rowconfigure(0, weight=1) #noqa
+            self.columnconfigure(0, weight=1) #noqa
+            
+            self.button = tk.Button(self, text="To cam one",
+                  command=lambda: master.switch_Cam(CamOne))
+            self.button.grid(row=1,column=0)
         
 if __name__ == "__main__":
-    app = SampleApp()
+    
+    getindexes = GetCamIndex()
+    indexes = getindexes.getIndex()
 
+    if indexes!=None:
+        app = CameraSwitchApp()
+    
